d1d1b0a756a0cbad2eca72c62f799a4fe06a6e88
Yared-Samuel <107871844+Yared-Samuel@users.noreply.github.com>
2024-09-26 21:30:42 +0300

    three cards done

--- app/components/ThreeCards.jsx
+++ app/components/ThreeCards.jsx
@@ -1,26 +1,68 @@
-import React from 'react'
-import { CiCoffeeBean } from 'react-icons/ci'
-import { SiCoffeescript } from 'react-icons/si'
-import { TbCoffee } from 'react-icons/tb'
-import './threeCards.css'
+import React from "react";
+import { CiCoffeeBean } from "react-icons/ci";
+import { TbCoffee } from "react-icons/tb";
+import { MdOutlineCoffeeMaker } from "react-icons/md";
+import "./threeCards.css";
+import Image from "next/image";
+import Parallax from "./Parallax";
 
 const ThreeCards = () => {
   return (
-    <div>
-        <h2 className="why">Why Choose BZNM?</h2>
+    <div className="three-cards-container">
+      <h2 className="why">Why BZNM?</h2>
       <div className="three-cards">
         <div className="one-of-three">
-          <CiCoffeeBean size={50} color="#754d1c" />
+          <div className="three-card-images-container">
+      <Parallax speed={1} className={"self-end"}>
+
+            <Image
+              className="three-card-image three-card-1"
+              src={"/images/bean1.jpg"}
+              alt="Coffee art image"
+              width={400}
+              height={300}
+            />
+            </Parallax>
+      <Parallax speed={4} className={"baseline"}>
+
+            <Image
+              className="three-card-image three-card-2"
+              src={"/images/bean6.jpg"}
+              alt="Coffee art image"
+              width={300}
+            height={400}
+              
+            />
+            </Parallax>
+          </div>
+
+          <CiCoffeeBean size={70} color="#1e3d2e" />
           <p>
-            At BZNM, we believe that great coffee starts with great baristas,
-            and we are committed to fostering the next generation of coffee
+            At BZNM, we believe that{" "}
+            <span className="card-span">
+              {" "}
+              great coffee starts with great baristas
+            </span>
+            , and we are committed to fostering the next generation of coffee
             professionals.
           </p>
         </div>
         <div className="one-of-three">
-          <SiCoffeescript size={50} color="#754d1c" />
+        <div className="three-card-images-container">
+        <Parallax speed={-1} className={"self-start"} scale={[1, 1.2]}>
+          <Image
+            className="three-card-image three-card-3"
+            src={"/images/bean3.jpg"}
+            alt="Coffee art image"
+            width={300}
+            height={400}
+          />
+          </Parallax>
+          </div>
+          <MdOutlineCoffeeMaker size={70} color="#1e3d2e" />
           <p>
-            What sets BZNM apart is that it offers a unique, one-of-a-kind class
+            What sets BZNM apart is that it offers a{" "}
+            <span className="card-span">unique and one-of-a-kind class </span>
             that is new to Ethiopia, making it an ideal starting point for
             anyone looking to begin their journey in the coffee industry.
             Whether you are aiming to start a career in coffee, open your own
@@ -30,18 +72,42 @@ const ThreeCards = () => {
           </p>
         </div>
         <div className="one-of-three">
-          <TbCoffee size={50} color="#754d1c" />
+        <div className="three-card-images-container">
+        <Parallax speed={-2} className={"self-end"}>
+
+          <Image
+            className="three-card-image three-card-5"
+            src={"/images/bean5.jpg"}
+            alt="Coffee art image"
+            width={300}
+            height={300}
+          />
+        </Parallax>
+        <Parallax speed={1.5} className={"self-end"}>
+
+          <Image
+            className="three-card-image three-card-6"
+            src={"/images/bean2.jpg"}
+            alt="Coffee art image"
+            width={400}
+              height={300}
+          />
+          </Parallax>
+
+          </div>
+          <TbCoffee size={70} color="#1e3d2e" />
           <p>
-            BZNM School of Barista and Training is a premier institution in
-            Ethiopia dedicated to providing world-class barista education and
-            coffee training. Our comprehensive curriculum equips aspiring
-            baristas and coffee enthusiasts with the knowledge, skills, and
-            confidence to excel in the dynamic world of specialty coffee.{" "}
+            BZNM School of Barista and Training is a{" "}
+            <span className="card-span"> premier institution in Ethiopia </span>{" "}
+            dedicated to providing world-class barista education and coffee
+            training. Our comprehensive curriculum equips aspiring baristas and
+            coffee enthusiasts with the knowledge, skills, and confidence to
+            excel in the dynamic world of specialty coffee.{" "}
           </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ThreeCards
+export default ThreeCards;
